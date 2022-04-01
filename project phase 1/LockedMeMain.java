c1c6a0bdfe2da506d1c2af3cb45e7816a34de64b
veeksha422 <102661091+veeksha422@users.noreply.github.com>
2022-04-01 13:18:03 +0530

    Update LockedMeMain.java

--- project phase 1/LockedMeMain.java
+++ project phase 1/LockedMeMain.java
@@ -7,7 +7,7 @@ public class LockedMeMain {
 		// Create "main" folder if not present in current folder structure
 		FileOperations.createMainFolderIfNotPresent("main");
 		
-		String veeksha;
+		
 		MenuOptions.printWelcomeScreen("LockedMe", veeksha);
 		
 		HandleOptions.handleWelcomeScreenInput();
